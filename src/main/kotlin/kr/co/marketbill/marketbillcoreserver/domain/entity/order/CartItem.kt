92097006660fcaf827b8f72153c67f6f4d74c3e2
Taewan Kim <terry960302@gmail.com>
2022-12-31 17:26:46 +0900

    :recycle: #53 장바구니 담기 upsert방식으로 변경

--- src/main/kotlin/kr/co/marketbill/marketbillcoreserver/domain/entity/order/CartItem.kt
+++ src/main/kotlin/kr/co/marketbill/marketbillcoreserver/domain/entity/order/CartItem.kt
@@ -1,14 +1,12 @@
 package kr.co.marketbill.marketbillcoreserver.domain.entity.order
 
 import kr.co.marketbill.marketbillcoreserver.constants.FlowerGrade
-import kr.co.marketbill.marketbillcoreserver.constants.SOFT_DELETE_CLAUSE
 import kr.co.marketbill.marketbillcoreserver.domain.entity.common.BaseTime
 import kr.co.marketbill.marketbillcoreserver.domain.entity.flower.Flower
 import kr.co.marketbill.marketbillcoreserver.domain.entity.user.User
 import kr.co.marketbill.marketbillcoreserver.util.EnumConverter
 import org.hibernate.annotations.SQLDelete
 import org.hibernate.annotations.Where
-import org.hibernate.annotations.WhereJoinTable
 import java.time.LocalDateTime
 import javax.persistence.*
 
@@ -19,7 +17,7 @@ import javax.persistence.*
 data class CartItem(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
+    var id: Long? = null,
 
     @ManyToOne
     @JoinColumn(name = "retailer_id")
