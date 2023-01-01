5da576de9fdb99406fe8a6a6cbdfee47dd11c31b
Taewan Kim <terry960302@gmail.com>
2023-01-01 18:08:39 +0900

    :recycle: #59 FlowerService 로깅 보완

--- src/main/kotlin/kr/co/marketbill/marketbillcoreserver/service/FlowerService.kt
+++ src/main/kotlin/kr/co/marketbill/marketbillcoreserver/service/FlowerService.kt
@@ -10,21 +10,28 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import java.time.LocalDate
-import java.util.*
 
 @Service
 class FlowerService {
     @Autowired
     private lateinit var flowerRepository: FlowerRepository
 
-
-    val logger: Logger = LoggerFactory.getLogger(FlowerService::class.java)
+    private val logger: Logger = LoggerFactory.getLogger(FlowerService::class.java)
+    private val className: String = this.javaClass.simpleName
 
     fun getFlowers(fromDate: LocalDate?, toDate: LocalDate?, keyword: String?, pageable: Pageable): Page<Flower> {
-        return flowerRepository.findAll(
-            FlowerSpecs.btwDates(fromDate, toDate).and(FlowerSpecs.nameLike(keyword)),
-            pageable
-        )
+        val executedFunc = object : Any() {}.javaClass.enclosingClass.name
+        try {
+            val flowers = flowerRepository.findAll(
+                FlowerSpecs.btwDates(fromDate, toDate).and(FlowerSpecs.nameLike(keyword)),
+                pageable
+            )
+            logger.info("$className.$executedFunc >> completed.")
+            return flowers
+        } catch (e: Exception) {
+            logger.error("$className.$executedFunc >> ${e.message}")
+            throw e
+        }
     }
 
 }
