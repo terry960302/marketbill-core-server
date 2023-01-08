b8afaafa2abfeb9f969a90f96beb9c601f05e718
Taewan Kim <terry960302@gmail.com>
2023-01-09 01:04:36 +0900

    :sparkles: #73 DailyOrderItem 엔티티 생성 및 Specs 제작

---
+++ src/main/kotlin/kr/co/marketbill/marketbillcoreserver/domain/repository/order/DailyOrderItemRepository.kt
@@ -0,0 +1,10 @@
+package kr.co.marketbill.marketbillcoreserver.domain.repository.order
+
+import kr.co.marketbill.marketbillcoreserver.domain.entity.order.DailyOrderItem
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.stereotype.Repository
+
+@Repository
+interface DailyOrderItemRepository : JpaRepository<DailyOrderItem, Long>, JpaSpecificationExecutor<DailyOrderItem> {
+}
