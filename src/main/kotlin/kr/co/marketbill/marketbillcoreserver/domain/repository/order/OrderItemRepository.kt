f427330f83ee4e3c58f592406ec62bd49ae4b1d6
Taewan Kim <terry960302@gmail.com>
2023-01-09 01:08:55 +0900

    :recycle: #73 orderItem 관련 코드 리팩토링

--- src/main/kotlin/kr/co/marketbill/marketbillcoreserver/domain/repository/order/OrderItemRepository.kt
+++ src/main/kotlin/kr/co/marketbill/marketbillcoreserver/domain/repository/order/OrderItemRepository.kt
@@ -1,11 +1,8 @@
 package kr.co.marketbill.marketbillcoreserver.domain.repository.order
 
-import kr.co.marketbill.marketbillcoreserver.constants.SOFT_DELETE_CLAUSE
 import kr.co.marketbill.marketbillcoreserver.domain.entity.order.OrderItem
-import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
