2d59f8032a46abc6fe4dce865b0996716c82c59d
Taewan Kim <terry960302@gmail.com>
2023-03-26 14:13:50 +0900

    :wrench: #100 디펜던시 버전 업그레이드

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,18 +33,18 @@ dependencies {
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
 	// jpa + db conn
-	implementation("org.postgresql:postgresql:42.5.1")
-	implementation("org.springframework:spring-jdbc:6.0.3")
+	implementation("org.postgresql:postgresql:42.5.4")
+	implementation("org.springframework:spring-jdbc:6.0.6")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
 	// graphql
-//	implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:latest.release")) -> SpringBoot 3.0 upgrade 필요
+//	implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:latest.release")) // SpringBoot 3.0.0 이상 호환되는 DGS 버전
 	implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:5.5.1")) // SpringBoot 2.7.5 호환되는 DGS 버전
 	implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
 	implementation("com.netflix.graphql.dgs:graphql-dgs-extended-scalars")
 
 	// actuator
-	implementation("org.springframework.boot:spring-boot-starter-actuator:3.0.1")
+	implementation("org.springframework.boot:spring-boot-starter-actuator:3.0.4")
 
 	// security
 	implementation("org.springframework.boot:spring-boot-starter-security")
@@ -56,12 +56,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 
 	// serialization
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
 	// kotest
-	testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
-	testImplementation("io.kotest:kotest-assertions-core:5.5.4")
-	testImplementation("io.kotest:kotest-property:5.5.4")
+	testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
+	testImplementation("io.kotest:kotest-assertions-core:5.5.5")
+	testImplementation("io.kotest:kotest-property:5.5.5")
 	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 
 	// queryDSL
