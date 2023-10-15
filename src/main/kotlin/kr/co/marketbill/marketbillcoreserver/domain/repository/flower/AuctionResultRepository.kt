822d50a4aded4930b150d1207116dee965b6914c
이재린 <leejaelin@gmail.com>
2023-10-15 19:01:55 +0900

    변수 할당 오류 수정

--- src/main/kotlin/kr/co/marketbill/marketbillcoreserver/domain/repository/flower/AuctionResultRepository.kt
+++ src/main/kotlin/kr/co/marketbill/marketbillcoreserver/domain/repository/flower/AuctionResultRepository.kt
@@ -1,9 +1,7 @@
 package kr.co.marketbill.marketbillcoreserver.domain.repository.flower
 
 import kr.co.marketbill.marketbillcoreserver.domain.entity.flower.AuctionResult
-import kr.co.marketbill.marketbillcoreserver.domain.entity.flower.AuctionResultWithGroupBy
 import kr.co.marketbill.marketbillcoreserver.domain.entity.flower.InterfaceAuctionResultWithGroupBy
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
@@ -15,16 +13,16 @@ interface AuctionResultRepository : JpaRepository<AuctionResult, Long>, JpaSpeci
     @Query(
         "SELECT " +
             "ROW_NUMBER() OVER (ORDER BY flower_name, flower_type_name, auction_date) AS id, " +
-            "flower_name as flowerName, " +
-            "flower_type_name as flowerTypeName, " +
-            "auction_date as auctionDate, " +
-            "min(retail_price) as retailPrice, " +
-            "STRING_AGG(id || '|' || flower_grade , ',') AS flowerGrade " +
+            "   flower_name as flowerName, " +
+            "   flower_type_name as flowerTypeName, " +
+            "   auction_date as auctionDate, " +
+            "   min(retail_price) as retailPrice, " +
+            "   STRING_AGG(id || '|' || flower_grade , ',') AS flowerGrade " +
             "FROM auction_results" +
-            "  WHERE deleted_at IS NULL" +
-            "  AND wholesaler_id = 16" +
-            "  AND retail_price IS NOT NULL" +
-            " group by flower_name, flower_type_name, auction_date"
+            "   WHERE deleted_at IS NULL" +
+            "   AND wholesaler_id = ?1" +
+            "   AND retail_price IS NOT NULL" +
+            " GROUP BY flower_name, flower_type_name, auction_date"
         , nativeQuery = true
     )
     fun findGroupByFlowerNameAndAuctionDate(wholesalerId: Long, pageable: Pageable): List<InterfaceAuctionResultWithGroupBy> = emptyList()
